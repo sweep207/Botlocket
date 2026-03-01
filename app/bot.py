face03ab21888be4e19863da22308158dee60219
Đặng Vũ <dangtoanvu07@gmail.com>
2026-03-02 06:18:37 +0700

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -6,6 +6,9 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, Cal
 from app.config import *
 from app import database as db
 from app.services import locket, nextdns
+from keep_alive import keep_alive
+
+keep_alive()  # Khởi động web server
 
 logger = logging.getLogger(__name__)
 
