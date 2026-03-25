f0f34a896cf2842305e3da04b3d1613ac8ad55ac
Đặng Vũ <dangtoanvu07@gmail.com>
2026-03-25 20:16:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import uvicorn
 from app.bot import run_bot
+from app.config import app  # Import biến app từ file config
+import threading
 
 if __name__ == "__main__":
-    run_bot()
+    # Chạy Bot Telegram ở một luồng riêng để không làm crash Web Server
+    threading.Thread(target=run_bot, daemon=True).start()
+    
+    # Chạy Web Server FastAPI
+    uvicorn.run(app, host="0.0.0.0", port=8080)
